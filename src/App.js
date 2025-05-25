0e5ec49a377f6c1282d834d9cff5352a509978bd
Abhishek-bramhawale <sanjivkumarbramhawale4992@gmail.com>
2025-05-25 23:14:21 +0530

    frontend basic part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './newogo2.png';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <nav className="nav">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Easy2share</h1>
+      </nav>
+      <div>
+        <div className="gradient-background">
+          <div className="gradient-sphere sphere-1"></div>
+          <div className="gradient-sphere sphere-2"></div>
+          <div className="gradient-sphere sphere-3"></div>
+          <div className="grid-overlay"></div>
+          <div className="noise-overlay"></div>
+        </div>
+
+        <div className="content-container">
+          <p className="tagline">Instant share. Anytime, anywhere</p>
+          <h2 className="tagline">Upload Files</h2>
+          <div className="fileinput-container">
+            <input className="fileinput" type="file" />
+            
+          </div>
+        </div>
+        <button  className="btn" >Upload</button>
+      </div>
     </div>
   );
 }
